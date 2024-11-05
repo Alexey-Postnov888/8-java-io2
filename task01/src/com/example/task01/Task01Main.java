3290d07b4a36227197e8c316b70a79a17776d2ef
Alexey Postnov <adpostnov@yandex.ru>
2024-11-06 00:32:10 +0500

    Постнов Алексей. Задание 01

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -10,13 +10,13 @@ public class Task01Main {
         System.out.println(extractSoundName(new File("task01/src/main/resources/3727.mp3")));
     }
 
-    public static String extractSoundName(File file) throws IOException, InterruptedException {
+    public static String extractSoundName(File file) throws IOException {
         ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c", "ffprobe -v error -of flat -show_format ", file.getAbsolutePath());
         builder.directory(new File("C:\\ffmpeg\\bin"));
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(builder.start().getInputStream()))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                if(line.contains("title")) {
+                if (line.contains("title")) {
                     return line.split("\"")[1];
                 }
             }
